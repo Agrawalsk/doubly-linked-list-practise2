a65a78dce63bbbb8daeaa9612f6bea778b92b6ad
Agrawalsk <shivkumar.bansal_cs22@gla.ac.in>
2024-03-24 21:25:33 +0530

    create displayDoublyLinkedList method to display the DoublyLinkedList.

--- src/main/java/Implementation/DoublyLinkedList.java
+++ src/main/java/Implementation/DoublyLinkedList.java
@@ -1,49 +1,62 @@
 package Implementation;
 
 public class DoublyLinkedList {
-    Node head;
-    Node tail;
+     Node head;
+     Node tail;
     int size = 0;
 
     boolean isEmpty() {
         return size == 0;
     }
-public void add(int data){
-        Node temp=new Node(data);
-        if(isEmpty()){
-           head=temp;
-           tail=temp;
-           size++;
-           return;
-        }
-        tail.next=temp;
-        temp.previous=tail;
-        tail=temp;
-        size++;
-}
 
-   static class Node {
+    public void add(int data) {
+        Node temp = new Node(data);
+        if (isEmpty()) {
+            head = temp;
+            tail = temp;
+            size++;
+            return;
+        }
+        tail.next = temp;
+        temp.previous = tail;
+        tail = temp;
+        size++;
+    }
+
+    void displayDoublyLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+            if (temp != null) System.out.print("<-> ");
+        }
+        System.out.println("-> null");
+    }
+
+    static class Node {
         int data;
         Node next;
         Node previous;
 
         public Node(int data) {
             this.data = data;
-            this.next=null;
-            this.previous=null;
+            this.next = null;
+            this.previous = null;
         }
     }
 }
 
-class Main{
+class Main {
     public static void main(String[] args) {
-        DoublyLinkedList doublyLinkedList=new DoublyLinkedList();
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
         doublyLinkedList.add(12);
         doublyLinkedList.add(13);
         doublyLinkedList.add(14);
         doublyLinkedList.add(15);
         doublyLinkedList.add(16);
         doublyLinkedList.add(17);
-        System.out.println(doublyLinkedList.size);
+//        System.out.println(doublyLinkedList.size);
+        doublyLinkedList.displayDoublyLinkedList(doublyLinkedList.head);
+
     }
 }
